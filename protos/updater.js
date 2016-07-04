9441df312f4f27ea0993d2c07780b81536e7c51f
Askwrite <dubstep17@yahoo.com>
2016-07-04 14:32:30 +0200

    Create updater.js

---
+++ protos/updater.js
@@ -0,0 +1,30 @@
+var fs = require("fs");
+var https = require("https");
+
+var baseUrl = "https://raw.githubusercontent.com/SteamDatabase/GameTracking/master/Protobufs/csgo/";
+var protos = [
+  "base_gcmessages.proto",
+  "steammessages.proto",
+  "cstrike15_gcmessages.proto",
+  "gcsdk_gcmessages.proto"
+];
+
+fs.readdir(__dirname, function(err, filenames) {
+  if (err) { 
+    return err; 
+  }
+
+  filenames.forEach(function(filename) {
+    console.log(filename);
+    if (filename != "protos.js" && filename != "updater.js") {
+      fs.unlinkSync(__dirname + "/" + filename);
+    }
+  });
+
+  protos.forEach(function(proto) {
+    var file = fs.createWriteStream(__dirname + "/" + proto);
+    https.get(baseUrl + proto, function(response) {
+      response.pipe(file);
+    });
+  });
+});
